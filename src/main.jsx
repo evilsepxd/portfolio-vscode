fdee6a65a8f6fa37bd7e80f6312c93ccd49c5b14
Peter Koval <evilsephy1@mail.ru>
2023-10-27 22:33:57 +0300

    пока rtk не используется

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
 
 import App from './components/app/App.jsx';
-import store from './components/app/store';
 
 import './style/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<App />
 	</React.StrictMode>,
 )
