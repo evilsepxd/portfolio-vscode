0296b59f0d24542695cf43fd52c20c7209c993c6
Peter Koval <evilsephy1@mail.ru>
2023-10-27 22:32:49 +0300

    начальные стили

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
-import App from './components/App/App.jsx';
+import App from './components/app/App.jsx';
 import store from './components/app/store';
 
-import './style/index.css'
+import './style/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
