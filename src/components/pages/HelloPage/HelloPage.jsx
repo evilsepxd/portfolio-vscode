dfb7c2444c50100b3058531ea7dbb6db9b078a2d
Peter Koval <evilsephy1@mail.ru>
2023-10-28 14:20:15 +0300

    верстка HelloPage без добавления игры

--- src/components/pages/HelloPage/HelloPage.jsx
+++ src/components/pages/HelloPage/HelloPage.jsx
@@ -1,6 +1,39 @@
+
+
+import bgBlurSrc from '../../../assets/img/bg-blurs.svg';
+
+import './helloPage.scss';
+
 const HelloPage = () => {
 	return (
-		<div className="hello page">hello</div>
+		<section className="hello page">
+			<div className="hello__titles">
+				<h2 className="hello__subtitle">
+					Hi all. I am
+				</h2>
+				<h1 className="hello__title">
+					Peter Koval
+				</h1>
+				<div className="hello__title-descr">
+					&gt; Front-end developer
+				</div>
+			</div>
+			<div className="hello__descrs">
+				<div className="hello__descr">
+					&#47;&#47; complete the game to continue <br/>
+					&#47;&#47; you can also see it on my Github page
+				</div>
+				<div className="hello__link">
+					<span className="const">const</span>
+					<span className="const-name"> githubLink </span>
+					=
+					<span className="const-value"> &quot;https://evilsepxd.github.io/snake/&quot;</span>
+				</div>
+			</div>
+			<div className="hello__game">
+				<img src={bgBlurSrc} alt="bg-blur" className="hello__bg" />
+			</div>
+		</section>
 	);
 }
 
