b8a00f1597a3ec67709833ad83d843b684dae656
Peter Koval <evilsephy1@mail.ru>
2023-11-09 21:17:56 +0300

    комментарий в InfoBlock (звездочки для каждой строки)

--- src/components/pages/AboutPage/InfoBlock/InfoBlock.jsx
+++ src/components/pages/AboutPage/InfoBlock/InfoBlock.jsx
@@ -9,9 +9,15 @@ import { useRef, useState, useEffect } from "react";
 function InfoBlock() {
 
 	const textRef = useRef(null);
+
 	const [lineNums, setLineNums] = useState([
 		<li className="info__line-num" key={0}>
 			1
+		</li>
+	]);
+	const [comments, setComments] = useState([
+		<li className="info__line-num" key={0}>
+			*
 		</li>
 	]);
 
@@ -20,15 +26,26 @@ function InfoBlock() {
 	useEffect(() => {
 		const lines = textRef.current.getClientRects().length;
 		const lineNums = [];
+		const comments = [];
+		let text = '*';
 		
-		for (let i = 0; i < lines; i++) {
+		for (let i = 0; i <= lines; i++) {
 			lineNums.push(
 				<li className="info__line-num" key={i}>
 					{ i + 1 }
 				</li>
 			);
+			if (i === 0) text = '/*';
+			else if (i === lines) text = '*/';
+			else text = '*';
+			comments.push(
+				<li className="info__line-comment" key={i}>
+					{ text }
+				</li>
+			);
 		}
 
+		setComments(comments);
 		setLineNums(lineNums);
 	}, []);
 
@@ -37,7 +54,11 @@ function InfoBlock() {
 			<ul className="info__nums">
 				{ lineNums }
 			</ul>
+			<ul className="info__comments">
+				{ comments }
+			</ul>
 			<div className="info__text">
+				
 				<span ref={textRef}>{ text }</span>
 			</div>
 			<Scroll />
