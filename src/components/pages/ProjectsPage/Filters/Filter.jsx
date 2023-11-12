ecd515368fff9f3fe20dc3bc02b5bef34e2fe28c
Peter Koval <evilsephy1@mail.ru>
2023-11-12 13:58:40 +0300

    верстка inputs в Filter + управляются с клавиатуры при нажатии Enter

--- src/components/pages/ProjectsPage/Filters/Filter.jsx
+++ src/components/pages/ProjectsPage/Filters/Filter.jsx
@@ -1,10 +1,38 @@
 
 
+import { useRef, useEffect, useState } from "react";
+
 function Filter({ name, iconSrc }) {
 
+	const itemRef = useRef(null);
+	const inputRef = useRef(null);
+	const [checked, setChecked] = useState(false);
+
+	const keyboardListener = (e) => {
+		if (document.activeElement === itemRef.current && e.key === 'Enter') {
+			setChecked(old => !old);
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('keydown', keyboardListener);
+
+		return () => {
+			document.removeEventListener('keydown', keyboardListener);
+		}
+	}, []);
+
 	return (
-		<label htmlFor={name} className="filters__item">
-			<input type="checkbox" name={name} />
+		<label htmlFor={name} className="filters__item" tabIndex={0} ref={itemRef} >
+			<input
+				type="checkbox"
+				name={name}
+				className="filters__input"
+				id={name}
+				ref={inputRef}
+				checked={checked}
+			/>
+			<div className="filters__input-custom"></div>
 			<div className="filters__item-inner">
 				<img src={iconSrc} alt="icon" className="filters__icon" />
 				<div className="filters__name">{ name }</div>
